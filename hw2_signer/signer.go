eac56899760af1bfee54b04c743d88e0cdbe9507
tooBusyNow <OldsmobileBuick54@yandex.ru>
2022-02-14 00:05:29 +0500

    trying to get the idea of linking chans

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,3 +1,28 @@
 package main
 
-// сюда писать код
+import "time"
+
+type chInf chan interface{}
+
+func ExecutePipeline(hashSignJobs ...job) {
+
+	var in, out chan interface{} = make(chInf, 1), make(chInf, 1)
+
+	for _, newJob := range hashSignJobs {
+		go newJob(in, out)
+	}
+
+	<-time.After(time.Second * 3)
+}
+
+func SingleHash(in, out chan interface{}) {
+
+}
+
+func MultiHash(in, out chan interface{}) {
+
+}
+
+func CombineResults(in, out chan interface{}) {
+
+}
